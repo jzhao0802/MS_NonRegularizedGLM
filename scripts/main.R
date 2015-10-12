e7e08d8571cbc2b48c13e74fbd66351806acfcf5
Lichao Wang <lichao.wang@uk.imshealth.com>
2015-10-12 18:25:19 +0100

    added: compare glm and glmnet on the combined relapse dataset, saving the test result whether the two ROCs are different

--- scripts/main.R
+++ scripts/main.R
@@ -2,8 +2,12 @@ library(glmnet)
 library(pROC)
 library(ROCR)
 
+rm(list=ls())
+
 source("functions/manualStratify.R")
 source("functions/computeWeights.R")
+
+
 
 kFoldsEval = 10
 log_lambda_seq <- seq(log(1e-4),log(1e4),length.out=100)
@@ -32,12 +36,12 @@ selected_alpha <- 0.85
 selected_lambda <- 0.01462
 # selected_lambda <- 0.01
 
-# fit the glmnet model
+# fit the glmnet and glm models
 
 folds <- manualStratify(y, kFoldsEval)
 
-predprobs_alldata <- matrix(data=-1, nrow=n_data, ncol=1)
-lambda_allfolds <- matrix(data=-1, nrow=kFoldsEval, ncol=1)
+predprobs_alldata_glmnet <- matrix(data=-1, nrow=n_data, ncol=1)
+predprobs_alldata_glm <- matrix(data=-1, nrow=n_data, ncol=1)
 
 timeStamp <- as.character(Sys.time())
 timeStamp <- gsub(":", ".", timeStamp)  # replace ":" by "."
@@ -59,39 +63,102 @@ for (iFold in 1:length(folds))
   
   weight_vec <- computeWeights(y_train)
   
-  fit <- glmnet(X_train, y_train, family="binomial", 
-                alpha=selected_alpha, lambda=lambda_seq)
-  predprobs_test <- predict(fit, newx=X_test, s=selected_lambda, type="response")
+  fit_glmnet <- glmnet(X_train, y_train, family="binomial", 
+                       alpha=selected_alpha, lambda=lambda_seq)
+  predprobs_test_glmnet <- 
+    predict(fit_glmnet, newx=X_test, s=selected_lambda, type="response")
+  
+  data_train <- data.frame(cbind(y_train,X_train))
+  colnames(data_train)[1] <- "y"
+  fit_glm <- glm(y ~ ., family="binomial", data=data_train)
+  data_test <- data.frame(cbind(y_test, X_test))
+  colnames(data_test)[1] <- "y"
+  predprobs_test_glm <- 
+    predict(fit_glm, newdata=data_test, type="response")
   
   # 
   
-  coefs_test <- predict(fit, newx=X_test, s=selected_lambda, type="coefficients")
+  coefs_test <- predict(fit_glmnet, newx=X_test, s=selected_lambda, type="coefficients")
   selected_var_names <- c(selected_var_names, rownames(coefs_test)[coefs_test[,1]!=0])
   
   # 
-  predprobs_alldata[test_ids] <- predprobs_test
+  predprobs_alldata_glmnet[test_ids] <- predprobs_test_glmnet
+  predprobs_alldata_glm[test_ids] <- predprobs_test_glm
   # lambda_allfolds[iFold, 1] <- fit$lambda.min
 }
 cat("\n")
 
 # test the AUC
 
-pred <- prediction(predictions=predprobs_alldata, labels=y)
-perf <- performance(pred, measure = "tpr", x.measure = "fpr") 
+pred_glmnet <- prediction(predictions=predprobs_alldata_glmnet, labels=y)
+perf_glmnet <- performance(pred_glmnet, measure = "tpr", x.measure = "fpr") 
 png(filename=paste(result_dir, study_name, "_roc_alpha", 
-                   selected_alpha, ".png", sep=""))
-plot(perf, col=rainbow(10))
+                   selected_alpha, "glmnet.png", sep=""))
+plot(perf_glmnet, col=rainbow(10))
 dev.off()
 
-roc_obj <- roc(response=as.vector(y), predictor=as.vector(predprobs_alldata))
-auc <- roc_obj$auc
-cat(paste("AUC: ", auc, "\n", sep=""))
+pred_glm <- prediction(predictions=predprobs_alldata_glm, labels=y)
+perf_glm <- performance(pred_glm, measure = "tpr", x.measure = "fpr") 
+png(filename=paste(result_dir, study_name, "_roc_alpha", 
+                   selected_alpha, "glm.png", sep=""))
+plot(perf_glm, col=rainbow(10))
+dev.off()
 
-ci_obj <- ci.auc(roc_obj)
+roc_obj_glmnet <- roc(response=as.vector(y), predictor=as.vector(predprobs_alldata_glmnet))
+auc_glmnet <- roc_obj_glmnet$auc
+cat(paste("AUC glmnet: ", auc_glmnet, "\n", sep=""))
 
-file_ciauc <- file(paste(result_dir, "ci_auc.csv", sep=""), "w")
-writeLines(paste(c(ci_obj[1], ci_obj[2], ci_obj[3]),collapse=","), file_ciauc)
-close(file_ciauc)
+ci_obj_glmnet <- ci.auc(roc_obj_glmnet)
+
+file_ciauc_glmnet <- file(paste(result_dir, "ci_auc_glmnet.csv", sep=""), "w")
+writeLines(paste(c(ci_obj_glmnet[1], ci_obj_glmnet[2], ci_obj_glmnet[3]),collapse=","), file_ciauc_glmnet)
+close(file_ciauc_glmnet)
+
+
+roc_obj_glm <- roc(response=as.vector(y), predictor=as.vector(predprobs_alldata_glm))
+auc_glm <- roc_obj_glm$auc
+cat(paste("AUC glm: ", auc_glm, "\n", sep=""))
+
+ci_obj_glm <- ci.auc(roc_obj_glm)
+
+file_ciauc_glm <- file(paste(result_dir, "ci_auc_glm.csv", sep=""), "w")
+writeLines(paste(c(ci_obj_glm[1], ci_obj_glm[2], ci_obj_glm[3]),collapse=","), file_ciauc_glm)
+close(file_ciauc_glm)
+
+
+
+hh <- roc.test(roc_obj_glm, roc_obj_glmnet)
+file_roc_compare <- file(paste(result_dir, "roc_compare.txt", sep=""), "w")
+for (i in 1:length(hh))
+{
+  if ((names(hh)[i] == "roc1") | (names(hh)[i] == "roc2"))
+    next
+  
+  writeLines(paste(names(hh)[i], ": ",sep=""), file_roc_compare)
+  if (is.null(names(hh[[i]])))
+  {
+    writeLines(paste(hh[[i]], "\n", sep=""), file_roc_compare)
+    # writeLines(paste(hh[[i]],"\n", sep=""), file_roc_compare)
+  }
+    
+  else
+  {
+    if (names(hh)[i] == "p.value")
+      writeLines(paste(hh[[i]][1], "\n", sep=""), file_roc_compare)
+    else
+    {
+      for (j in 1:length(hh[[i]]))
+      {
+        writeLines(paste(names(hh[[i]][j]), ": ", hh[[i]][j], sep=""), file_roc_compare)
+      }
+      writeLines("\n", file_roc_compare)
+    }
+  }
+}
+
+writeLines("Are the two ROCs paired?\n", file_roc_compare)
+writeLines(paste(are.paired(roc_obj_glm, roc_obj_glmnet), "\n"), file_roc_compare)
+close(file_roc_compare)
 
 
 # use selected features for non-regularised LR
